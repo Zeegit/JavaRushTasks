4384a80bd1b47846e1abb1405227a949a9db68bc
Zeegit <sergey.zemskov@gmail.com>
2020-09-16 14:40:34 +0300

    15.09.2020

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    public static Hippodrome game;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void move() {
+        for (Horse horse : horses) {
+            horse.move();
+        }
+    }
+
+    public void print() {
+        for (Horse horse : horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public Horse getWinner() {
+        Horse winner = horses.get(0);
+        for (int i = 1; i < horses.size(); i++) {
+            if (horses.get(i).getDistance() > winner.getDistance()) {
+                winner = horses.get(i);
+            }
+        }
+        return winner;
+    }
+
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
+    public static void main(String[] args) {
+        List<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("Yanka", 3, 0));
+        horses.add(new Horse("Bella", 3, 0));
+        horses.add(new Horse("Umka", 3, 0));
+        game = new Hippodrome(horses);
+        game.run();
+        game.printWinner();
+    }
+}
